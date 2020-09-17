4e136c2ae71e8e867a49fe51acfb95a1c51b8427
Swai Collins <swailerc23@gmail.com>
2020-09-17 22:26:43 +0300

    Data types examples

---
+++ src/DataTypes.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val str: String
+    str="Welcome to kotlin"
+    val name: String = "Collins"
+    val age: Int =  25
+
+    println("==== User info ====")
+    println("Name: $name")
+    println("Age: $age")
+}
