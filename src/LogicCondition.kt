9ea700ff6835f22e2e29759233c478faf13b78ef
Swai Collins <swailerc23@gmail.com>
2020-09-19 07:30:40 +0300

    Logic Condition example

---
+++ src/LogicCondition.kt
@@ -0,0 +1,21 @@
+/*
+And Gate(&&)
+    True && True = True
+    True && False = False
+    False && True = False
+    False && False = False
+
+OR Gate(!!)
+    True !! True = True
+    True !! False = True
+    False !! True = True
+    False !! False = False
+NOT Gate(!)
+    True = False
+    False = True
+ */
+
+fun main(){
+    val age = 56
+    println(age in 19..72)
+}
