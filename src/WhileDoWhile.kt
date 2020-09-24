647d29a953e4882eb7d89c19a37105b49bd1491c
Swai Collins <swailerc23@gmail.com>
2020-09-24 19:57:38 +0300

    While Do Loop

---
+++ src/WhileDoWhile.kt
@@ -0,0 +1,15 @@
+fun main(){
+    //While Loop
+    var i = 1
+    while (i<=10){
+        println("Counter $i")
+        i += 2
+    }
+
+    //Do While Loops
+    var x = 1
+    do {
+        println("Counter $x")
+        x += 2
+    }while (x<=10)
+}
