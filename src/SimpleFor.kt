64f69845fbb7e85546b10277f6d2293ce500ba50
Swai Collins <swailerc23@gmail.com>
2020-09-24 19:50:05 +0300

    Simple For Loop

---
+++ src/SimpleFor.kt
@@ -0,0 +1,11 @@
+fun main(){
+    //Increment
+    for(i in 0..30 step 2){
+        println("Number is  $i")
+    }
+
+    //Decrement
+    for(i in 30 downTo 0 step 2){
+        println("Number is  $i")
+    }
+}
