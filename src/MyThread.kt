4de189746b99c8910154bb5b3f9e1780e1e5760e
Swai Collins <swailerc23@gmail.com>
2020-09-26 20:07:24 +0300

    MultiThreading

---
+++ src/MyThread.kt
@@ -0,0 +1,40 @@
+import java.lang.Exception
+
+
+fun main(args:Array<String>){
+    var t1 = thread("thread1")
+    t1.start()
+
+    var t2 = thread("thread2")
+    t2.start()
+
+    var t3 = thread("thread3")
+    t3.start()
+    t3.join()
+    println("Thread started")
+
+
+}
+
+class thread():Thread(){
+    var ThreadName:String = ""
+
+    constructor(ThreadName:String):this(){
+        this.ThreadName=ThreadName
+        println(this.ThreadName+ "is started")
+    }
+
+    override fun run() {
+        var count = 0
+        while (count<10){
+            println(this.ThreadName+ "Count: $count")
+            count++
+
+            try {
+                Thread.sleep(1000)
+            }catch (ex:Exception){
+                print(ex.message)
+            }
+        }
+    }
+}
